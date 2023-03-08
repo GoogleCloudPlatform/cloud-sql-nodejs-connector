3c3bb7fbb1b116cc081ac88691b9fd5ed44f50fe
Ruy Adorno <ruyadorno@google.com>
2023-03-08 14:10:57 -0500

    chore: add esm end-to-end connection test
    Adds a connect end-to-end test that uses the ESM module system.

---
+++ system-test/pg-connect.mjs
@@ -0,0 +1,40 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import t from 'tap';
+import pg from 'pg';
+import {Connector} from '@google-cloud/cloud-sql-connector';
+const {Client} = pg;
+
+t.test('open connection and retrieves standard pg tables', async t => {
+  const connector = new Connector();
+  const clientOpts = await connector.getOptions({
+    instanceConnectionName: process.env.POSTGRES_CONNECTION_NAME,
+    type: 'PUBLIC',
+  });
+  const client = new Client({
+    ...clientOpts,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DB,
+  });
+  client.connect();
+  const result = await client.query('SELECT * FROM pg_catalog.pg_tables;');
+
+  t.same(result.command, 'SELECT', 'should list correct command in response');
+  t.ok(result.rowCount > 0, 'should be able to retrieve list of tables');
+
+  await client.end();
+  connector.close();
+});
