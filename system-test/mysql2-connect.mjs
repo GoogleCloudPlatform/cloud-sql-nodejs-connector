93168049995520bc290368b0530fd768d052db38
Ruy Adorno <ruyadorno@google.com>
2023-03-28 17:59:01 -0400

    feat: add mysql support
    This changeset introduces end-to-end tests for MySQL support via the
    `mysql2` driver along with base README documentation on how to use the
    connector with a MySQL Cloud SQL instance.
    
    Fixes: https://github.com/GoogleCloudPlatform/cloud-sql-nodejs-connector/issues/65

--- system-test/mysql2-connect.mjs
+++ system-test/mysql2-connect.mjs
@@ -12,31 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const t = require('tap');
-const pg = require('pg');
-const {Connector} = require('@google-cloud/cloud-sql-connector');
-const {Client} = pg;
+import t from 'tap';
+import mysql from 'mysql2/promise';
+import {Connector} from '@google-cloud/cloud-sql-connector';
 
-t.test('open connection and retrieves standard pg tables', async t => {
+t.test('open connection and run basic mysql commands', async t => {
   const connector = new Connector();
   const clientOpts = await connector.getOptions({
-    instanceConnectionName: process.env.POSTGRES_CONNECTION_NAME,
+    instanceConnectionName: process.env.MYSQL_CONNECTION_NAME,
     type: 'PUBLIC',
   });
-  const client = new Client({
+  const conn = await mysql.createConnection({
     ...clientOpts,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASS,
-    database: process.env.POSTGRES_DB,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
   });
-  client.connect();
 
-  const {
-    rows: [result],
-  } = await client.query('SELECT NOW();');
-  const returnedDate = result['now'];
+  const [[result]] = await conn.query('SELECT NOW();');
+  const returnedDate = result['NOW()'];
   t.ok(returnedDate.getTime(), 'should have valid returned date object');
 
-  await client.end();
+  await conn.end();
   connector.close();
 });
