2d09b2393b502fdc184d60f302224221299c0d5e
Dinko Osrecki <dinko.osrecki@emarsys.com>
2023-09-04 14:38:32 +0200

    docs: add knex examples

---
+++ examples/pg-knex.mjs
@@ -0,0 +1,42 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Connector} from '@google-cloud/cloud-sql-connector';
+import knex from 'knex';
+
+const main = async () => {
+  const connector = new Connector();
+  const clientOpts = await connector.getOptions({
+    instanceConnectionName: 'my-project:region:my-instance',
+    ipType: 'PUBLIC',
+    authType: 'IAM',
+  });
+
+  const database = knex({
+    client: 'pg',
+    connection: {
+      ...clientOpts,
+      user: 'my-service-account@my-project.iam',
+      database: 'my-database',
+    },
+  });
+
+  const result = await database.first(database.raw('NOW() AS now'));
+  console.log(`Current datetime: ${result['now']}`);
+
+  await database.destroy();
+  connector.close();
+};
+
+main();
