8434d03e32ebbb4153d80827ec0998b73e8402f3
Jonathan Hess <hessjc@google.com>
2025-02-24 16:55:57 -0700

    test: E2E tests for domain name

--- system-test/pg-connect.mjs
+++ system-test/pg-connect.mjs
@@ -17,53 +17,61 @@ import pg from 'pg';
 import {Connector} from '@google-cloud/cloud-sql-connector';
 const {Client} = pg;
 
+
 t.test('open connection and retrieves standard pg tables', async t => {
   const connector = new Connector();
   const clientOpts = await connector.getOptions({
-    instanceConnectionName: process.env.POSTGRES_CONNECTION_NAME,
-    ipType: 'PUBLIC',
-    authType: 'PASSWORD',
+    instanceConnectionName: String(process.env.POSTGRES_CONNECTION_NAME),
   });
   const client = new Client({
     ...clientOpts,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASS,
-    database: process.env.POSTGRES_DB,
+    user: String(process.env.POSTGRES_USER),
+    password: String(process.env.POSTGRES_PASS),
+    database: String(process.env.POSTGRES_DB),
   });
-  client.connect();
+  t.after(async () => {
+    try {
+      await client.end();
+    } finally {
+      connector.close();
+    }
+  });
+
+  await client.connect();
 
   const {
     rows: [result],
   } = await client.query('SELECT NOW();');
   const returnedDate = result['now'];
   t.ok(returnedDate.getTime(), 'should have valid returned date object');
-
-  await client.end();
-  connector.close();
 });
 
 t.test('open IAM connection and retrieves standard pg tables', async t => {
   const connector = new Connector();
   const clientOpts = await connector.getOptions({
-    instanceConnectionName: process.env.POSTGRES_CONNECTION_NAME,
-    ipType: 'PUBLIC',
-    authType: 'IAM',
+    instanceConnectionName: String(process.env.POSTGRES_CONNECTION_NAME),
+    ipType: "PUBLIC",
+    authType: "IAM",
   });
   const client = new Client({
     ...clientOpts,
-    user: process.env.POSTGRES_IAM_USER,
-    database: process.env.POSTGRES_DB,
+    user: String(process.env.POSTGRES_USER_IAM_NODE),
+    database: String(process.env.POSTGRES_DB),
   });
-  client.connect();
+  t.after(async () => {
+    try {
+      await client.end();
+    } finally {
+      connector.close();
+    }
+  });
+  await client.connect();
 
   const {
     rows: [result],
   } = await client.query('SELECT NOW();');
   const returnedDate = result['now'];
   t.ok(returnedDate.getTime(), 'should have valid returned date object');
-
-  await client.end();
-  connector.close();
 });
 
 t.test(
@@ -71,26 +79,29 @@ t.test(
   async t => {
     const connector = new Connector();
     const clientOpts = await connector.getOptions({
-      instanceConnectionName: process.env.POSTGRES_CAS_CONNECTION_NAME,
-      ipType: 'PUBLIC',
-      authType: 'PASSWORD',
+      instanceConnectionName: String(process.env.POSTGRES_CAS_CONNECTION_NAME),
     });
     const client = new Client({
       ...clientOpts,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_CAS_PASS,
-      database: process.env.POSTGRES_DB,
+      user: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_CAS_PASS),
+      database: String(process.env.POSTGRES_DB),
     });
-    client.connect();
+    t.after(async () => {
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+      }
+    });
+
+    await client.connect();
 
     const {
       rows: [result],
     } = await client.query('SELECT NOW();');
     const returnedDate = result['now'];
     t.ok(returnedDate.getTime(), 'should have valid returned date object');
-
-    await client.end();
-    connector.close();
   }
 );
 
@@ -109,13 +120,82 @@ t.test(
       password: String(process.env.POSTGRES_CUSTOMER_CAS_PASS),
       database: String(process.env.POSTGRES_DB),
     });
-    client.connect();
+    t.after(async () => {
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+      }
+    });
+
+    await client.connect();
     const {
       rows: [result],
     } = await client.query('SELECT NOW();');
     const returnedDate = result['now'];
     t.ok(returnedDate.getTime(), 'should have valid returned date object');
-    await client.end();
-    connector.close();
+  }
+);
+
+t.test(
+  'open connection to Domain Name instance retrieves standard pg tables',
+  async t => {
+    const connector = new Connector();
+    const clientOpts = await connector.getOptions({
+      domainName: String(process.env.POSTGRES_CUSTOMER_CAS_DOMAIN_NAME),
+    });
+    const client = new Client({
+      ...clientOpts,
+      user: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_CUSTOMER_CAS_PASS),
+      database: String(process.env.POSTGRES_DB),
+    });
+    t.after(async () => {
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+      }
+    });
+
+    await client.connect();
+    const {
+      rows: [result],
+    } = await client.query('SELECT NOW();');
+    const returnedDate = result['now'];
+    t.ok(returnedDate.getTime(), 'should have valid returned date object');
+  }
+);
+
+t.test(
+  'open connection to Domain Name invalid domain name rejects connection',
+  async t => {
+    const connector = new Connector();
+    const clientOpts = await connector.getOptions({
+      domainName: String(process.env.POSTGRES_CUSTOMER_CAS_INVALID_DOMAIN_NAME),
+    });
+    const client = new Client({
+      ...clientOpts,
+      user: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_CUSTOMER_CAS_PASS),
+      database: String(process.env.POSTGRES_DB),
+    });
+    t.after(async () => {
+      console.log('Ending...');
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+        console.log('Ended...');
+      }
+    });
+    try {
+      await client.connect();
+      t.fail('Should throw exception');
+    } catch (e) {
+      t.same(e.code, 'ERR_TLS_CERT_ALTNAME_INVALID');
+    } finally {
+      t.end();
+    }
   }
 );
--- system-test/pg-connect.ts
+++ system-test/pg-connect.ts
@@ -32,16 +32,21 @@ t.test('open connection and retrieves standard pg tables', async t => {
     password: String(process.env.POSTGRES_PASS),
     database: String(process.env.POSTGRES_DB),
   });
-  client.connect();
+  t.after(async () => {
+    try {
+      await client.end();
+    } finally {
+      connector.close();
+    }
+  });
+
+  await client.connect();
 
   const {
     rows: [result],
   } = await client.query('SELECT NOW();');
   const returnedDate = result['now'];
   t.ok(returnedDate.getTime(), 'should have valid returned date object');
-
-  await client.end();
-  connector.close();
 });
 
 t.test('open IAM connection and retrieves standard pg tables', async t => {
@@ -53,19 +58,23 @@ t.test('open IAM connection and retrieves standard pg tables', async t => {
   });
   const client = new Client({
     ...clientOpts,
-    user: String(process.env.POSTGRES_IAM_USER),
+    user: String(process.env.POSTGRES_USER_IAM_NODE),
     database: String(process.env.POSTGRES_DB),
   });
-  client.connect();
+  t.after(async () => {
+    try {
+      await client.end();
+    } finally {
+      connector.close();
+    }
+  });
+  await client.connect();
 
   const {
     rows: [result],
   } = await client.query('SELECT NOW();');
   const returnedDate = result['now'];
   t.ok(returnedDate.getTime(), 'should have valid returned date object');
-
-  await client.end();
-  connector.close();
 });
 
 t.test(
@@ -81,16 +90,21 @@ t.test(
       password: String(process.env.POSTGRES_CAS_PASS),
       database: String(process.env.POSTGRES_DB),
     });
-    client.connect();
+    t.after(async () => {
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+      }
+    });
+
+    await client.connect();
 
     const {
       rows: [result],
     } = await client.query('SELECT NOW();');
     const returnedDate = result['now'];
     t.ok(returnedDate.getTime(), 'should have valid returned date object');
-
-    await client.end();
-    connector.close();
   }
 );
 
@@ -109,13 +123,82 @@ t.test(
       password: String(process.env.POSTGRES_CUSTOMER_CAS_PASS),
       database: String(process.env.POSTGRES_DB),
     });
-    client.connect();
+    t.after(async () => {
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+      }
+    });
+
+    await client.connect();
     const {
       rows: [result],
     } = await client.query('SELECT NOW();');
     const returnedDate = result['now'];
     t.ok(returnedDate.getTime(), 'should have valid returned date object');
-    await client.end();
-    connector.close();
+  }
+);
+
+t.test(
+  'open connection to Domain Name instance retrieves standard pg tables',
+  async t => {
+    const connector = new Connector();
+    const clientOpts = await connector.getOptions({
+      domainName: String(process.env.POSTGRES_CUSTOMER_CAS_DOMAIN_NAME),
+    });
+    const client = new Client({
+      ...clientOpts,
+      user: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_CUSTOMER_CAS_PASS),
+      database: String(process.env.POSTGRES_DB),
+    });
+    t.after(async () => {
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+      }
+    });
+
+    await client.connect();
+    const {
+      rows: [result],
+    } = await client.query('SELECT NOW();');
+    const returnedDate = result['now'];
+    t.ok(returnedDate.getTime(), 'should have valid returned date object');
+  }
+);
+
+t.test(
+  'open connection to Domain Name invalid domain name rejects connection',
+  async t => {
+    const connector = new Connector();
+    const clientOpts = await connector.getOptions({
+      domainName: String(process.env.POSTGRES_CUSTOMER_CAS_INVALID_DOMAIN_NAME),
+    });
+    const client = new Client({
+      ...clientOpts,
+      user: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_CUSTOMER_CAS_PASS),
+      database: String(process.env.POSTGRES_DB),
+    });
+    t.after(async () => {
+      console.log('Ending...');
+      try {
+        await client.end();
+      } finally {
+        connector.close();
+        console.log('Ended...');
+      }
+    });
+    try {
+      await client.connect();
+      t.fail('Should throw exception');
+    } catch (e) {
+      t.same(e.code, 'ERR_TLS_CERT_ALTNAME_INVALID');
+    } finally {
+      t.end();
+    }
   }
 );
