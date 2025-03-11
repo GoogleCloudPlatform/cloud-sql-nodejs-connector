0e4853a6149d9ef8270cb71976a217443650384f
Jonathan Hess (he/him) <103529393+hessjcg@users.noreply.github.com>
2025-03-11 10:58:47 -0600

    refactor: Improve the refresh to allow connectors to close, part of #421. (#424)
    Add CloudSqlInstance.close() to stop the refresh cycle permanently.
    
    Change CloudSqlInstance.forceRefresh() so that it schedules a refresh and returns immediately.
    This should not return a promise. Update test cases to account for changes.
    
    Part of #421

--- src/connector.ts
+++ src/connector.ts
@@ -230,8 +230,8 @@ export class Connector {
             serverCaMode,
             dnsName,
           });
-          tlsSocket.once('error', async () => {
-            await cloudSqlInstance.forceRefresh();
+          tlsSocket.once('error', () => {
+            cloudSqlInstance.forceRefresh();
           });
           tlsSocket.once('secureConnect', async () => {
             cloudSqlInstance.setEstablishedConnection();
@@ -333,7 +333,7 @@ export class Connector {
   // Also clear up any local proxy servers and socket connections.
   close(): void {
     for (const instance of this.instances.values()) {
-      instance.cancelRefresh();
+      instance.close();
     }
     for (const server of this.localProxies) {
       server.close();
